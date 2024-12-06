424bafb24b93b3c84b7f988e0cb0464b2554b2cc
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2024-12-07 00:33:07 +0545

    SNACKS
    - awesome
    - plugin

--- lua/custom/plugins/project.lua
+++ lua/custom/plugins/project.lua
@@ -2,7 +2,7 @@ return {
   'ahmedkhalf/project.nvim',
   config = function()
     require('project_nvim').setup {
-      patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', 'mvnw', 'mvnw.cmd' },
+      patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', 'mvnw', 'mvnw.cmd', 'pom.xml' },
       outputs = {
         require('projector_dbee').OutputBuilder:new(),
       },
